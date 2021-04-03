55e97074cfe2836def7b3165ce83f135ec764046
alexandrethm <33730312+alexandrethm@users.noreply.github.com>
2021-04-03 20:21:21 +0200

    implement our BNN - does not behave exactly like blitz: numerical differences?

--- src/models.py
+++ src/models.py
@@ -1,7 +1,136 @@
 import torch
-from blitz.modules import BayesianLinear
-from blitz.utils import variational_estimator
+
+# from blitz.modules import BayesianLinear
+from blitz.modules import PriorWeightDistribution, TrainableRandomDistribution
 from torch import nn
+from torch.distributions import Normal, Categorical, MixtureSameFamily
+import torch.nn.functional as F
+
+
+class BayesianLinear(nn.Module):
+    def __init__(
+        self,
+        dim_in,
+        dim_out,
+        prior_sigma_1=0.1,
+        prior_sigma_2=0.4,
+        prior_pi=1,
+        posterior_mu_init=0,
+        posterior_rho_init=-7.0,
+    ):
+        super().__init__()
+        # Parameters theta=[mu,rho] of variational posterior q(w|theta): for weights...
+        # todo: init mu/rho to match prior instead??
+        # Variational weight parameters and sample
+        self.mu = nn.Parameter(
+            torch.Tensor(dim_out, dim_in).normal_(posterior_mu_init, 0.1)
+        )
+        self.rho = nn.Parameter(
+            torch.Tensor(dim_out, dim_in).normal_(posterior_rho_init, 0.1)
+        )
+        # ... and biases
+        self.mu_bias = nn.Parameter(
+            torch.Tensor(dim_out).normal_(posterior_mu_init, 0.1)
+        )
+        self.rho_bias = nn.Parameter(
+            torch.Tensor(dim_out).normal_(posterior_rho_init, 0.1)
+        )
+
+        # Priors (as BBP paper): gaussian mixture model of two 1D normal distributions
+        mix = Categorical(torch.Tensor([prior_pi, 1 - prior_pi]))
+        comp = Normal(torch.zeros(2), torch.Tensor([prior_sigma_1, prior_sigma_2]))
+        self.prior = MixtureSameFamily(mix, comp)
+
+        self.log_variational_posterior = 0.0
+        self.log_prior = 0.0
+
+    def forward(self, x):
+        # Sample the weights and forward it
+        # perform all operations in the forward rather than in __init__ (including log[1+exp(rho)])
+        variational_posterior = Normal(self.mu, torch.log1p(torch.exp(self.rho)))
+        variational_posterior_bias = Normal(
+            self.mu_bias, torch.log1p(torch.exp(self.rho_bias))
+        )
+        w = variational_posterior.rsample()
+        b = variational_posterior_bias.rsample()
+
+        # Get the log prob
+        self.log_variational_posterior = (variational_posterior.log_prob(w)).sum() + (
+            variational_posterior_bias.log_prob(b)
+        ).sum()
+        self.log_prior = (self.prior.log_prob(w) + self.prior.log_prob(b)).sum()
+        return F.linear(x, w, b)
+
+
+class VariationalEstimator(nn.Module):
+    def sample_elbo(
+        self, inputs, labels, criterion, sample_nbr: int, complexity_cost_weight: float
+    ):
+        loss = 0
+        for _ in range(sample_nbr):
+            outputs = self(inputs)
+            loss += criterion(outputs, labels)
+            loss += self.kl_div() * complexity_cost_weight
+        return loss / sample_nbr
+
+    def kl_div(self):
+        r"""
+        Computes empirical estimate of
+            KL[q(w|mu,rho) || P(w)] = KL[variational posterior || prior]
+
+        .. math::
+            \sum_i \log q(w|\mu,\rho) - \log P(w)
+        where w is sampled from q
+
+        Returns:
+
+        """
+        kl_divergence = 0
+        for module in self.modules():
+            if isinstance(module, BayesianLinear):
+                kl_divergence += module.log_variational_posterior - module.log_prior
+        return kl_divergence
+
+
+class RBF(nn.Module):
+    def forward(self, x):
+        return torch.exp(-(x ** 2))
+
+
+class BayesianMLP(VariationalEstimator):
+    def __init__(self, dim_in, dim_out, dim_h, prior_sigma_1, prior_sigma_2, prior_pi):
+        super().__init__()
+        self.model = nn.Sequential(
+            BayesianLinear(
+                dim_in,
+                dim_h,
+                prior_sigma_1=prior_sigma_1,
+                prior_sigma_2=prior_sigma_2,
+                prior_pi=prior_pi,
+            ),
+            nn.ReLU(),
+            BayesianLinear(
+                dim_h,
+                dim_out,
+                prior_sigma_1=prior_sigma_1,
+                prior_sigma_2=prior_sigma_2,
+                prior_pi=prior_pi,
+            ),
+        )
+
+    def forward(self, x, num_samples=1):
+        if num_samples == 1:
+            # return a single prediction
+            return self.model(x)
+
+        else:
+            # make num_samples predictions with different weights
+            predictions = [self(x) for _ in range(num_samples)]
+
+            predictions = torch.stack(predictions)  # (num_samples, *)
+            mean = predictions.mean(dim=0)
+            std = predictions.std(dim=0)
+            return mean, std
 
 
 class RegularMLP(nn.Module):
@@ -17,38 +146,3 @@ class RegularMLP(nn.Module):
         if num_samples == 1:
             # return a single prediction
             return self.model(x)
-
-
-@variational_estimator
-class BayesianMLP(nn.Module):
-    def __init__(self, dim_in, dim_out, dim_h, prior_sigma_1, prior_sigma_2, prior_pi):
-        super().__init__()
-        self.blinear1 = BayesianLinear(
-            dim_in,
-            dim_h,
-            prior_sigma_1=prior_sigma_1,
-            prior_sigma_2=prior_sigma_2,
-            prior_pi=prior_pi,
-        )
-        self.blinear2 = BayesianLinear(
-            dim_h,
-            dim_out,
-            prior_sigma_1=prior_sigma_1,
-            prior_sigma_2=prior_sigma_2,
-            prior_pi=prior_pi,
-        )
-
-    def forward(self, x, num_samples=1):
-        if num_samples == 1:
-            # return a single prediction
-            x = torch.relu(self.blinear1(x))
-            return self.blinear2(x)
-
-        else:
-            # make num_samples predictions with different weights
-            predictions = [self(x) for _ in range(num_samples)]
-
-            predictions = torch.stack(predictions)  # (num_samples, *)
-            mean = predictions.mean(dim=0)
-            std = predictions.std(dim=0)
-            return mean, std
--- src/train.py
+++ src/train.py
@@ -14,60 +14,23 @@ from torch.utils.tensorboard import SummaryWriter
 
 import torch.nn.functional as F
 from datasets import get_toy_data
-from models import BayesianMLP, RegularMLP
+from models import BayesianMLP, RegularMLP, VariationalEstimator
 from utils import plot_results, evaluate_regression
 import optuna
 
 
-def log_metrics(writer, model, data, loss_history: List[float], epoch: int):
-    x_train, y_train, x_val, y_val, x_all, y_all = data
-    writer.add_scalar("loss", np.mean(loss_history), epoch)
-    y_pred_train, _ = model(x_train, num_samples=10)
-    mse_train = F.mse_loss(y_pred_train, y_train).mean().item()
-    writer.add_scalar("mse_train", mse_train, epoch)
-    y_pred_val, _ = model(x_val, num_samples=10)
-    mse_val = F.mse_loss(y_pred_val, y_val).mean().item()
-    writer.add_scalar("mse_val", mse_val, epoch)
-
-    if epoch % 500 == 0:
-        # plot results with all ground truth
-        y_pred, std_pred = model(x_all, num_samples=10)
-        fig = plot_results(x_train, y_train, x_all, y_all, y_pred, std_pred)
-        writer.add_figure("regression", fig, epoch)
-
-    return mse_val
-
-
-def objective(trial: optuna.Trial):
-    dim_h = 512
-    prior_sigma_1 = trial.suggest_float("prior_sigma_1", low=1e-3, high=10.0, log=True)
-    prior_sigma_2 = trial.suggest_float("prior_sigma_2", low=1e-3, high=10.0, log=True)
-    prior_pi = trial.suggest_float("prior_pi", low=0.0, high=1.0)
-
-    log_dir = (
-        project_dir
-        / f"runs/{trial.study.study_name}/trial_{trial.number}-dim_h_{dim_h}-"
-        f"s1_{prior_sigma_1:.2f}-s2_{prior_sigma_2:.2f}-pi_{prior_pi:.2f}"
-    )
+def train(
+    model: VariationalEstimator,
+    optimizer: torch.optim.Optimizer,
+    dataloader_train,
+    n_epochs: int,
+    log_dir: str,
+    evaluate_func,
+    evaluate_data,
+):
     writer = SummaryWriter(str(log_dir))
-
-    x_train, y_train, x_val, y_val, x_all, y_all = get_toy_data()
-    dataloader_train = DataLoader(
-        TensorDataset(x_train, y_train), batch_size=16, shuffle=True
-    )
-
-    model = BayesianMLP(
-        dim_in=1,
-        dim_out=1,
-        dim_h=dim_h,
-        prior_sigma_1=prior_sigma_1,
-        prior_sigma_2=prior_sigma_2,
-        prior_pi=prior_pi,
-    )
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-
     global_step = 0
-    for epoch in range(1000):
+    for epoch in range(n_epochs):
         loss_history = []
         M = len(dataloader_train)
         # M = len(x_train)
@@ -87,25 +50,113 @@ def objective(trial: optuna.Trial):
             global_step += 1
             loss_history.append(loss.item())
 
-        mse_val = log_metrics(
-            writer,
-            model,
-            (x_train, y_train, x_val, y_val, x_all, y_all),
-            loss_history,
-            epoch,
-        )
-        trial.report(mse_val, epoch)
-        if trial.should_prune():
-            raise optuna.TrialPruned()
+        with torch.no_grad():
+            # todo: use weight means instead of samples
+            val_metric = evaluate_func(
+                model,
+                evaluate_data,
+                writer,
+                loss_history,
+                epoch,
+            )
+    return val_metric
+
+
+# --- 1D regression
+def eval_1d_regression(model, data, writer, loss_history: List[float], epoch: int):
+    x_train, y_train, x_val, y_val, x_all, y_all = data
+    writer.add_scalar("loss", np.mean(loss_history), epoch)
+    y_pred_train, _ = model(x_train, num_samples=10)
+    mse_train = F.mse_loss(y_pred_train, y_train).mean().item()
+    writer.add_scalar("mse_train", mse_train, epoch)
+    y_pred_val, _ = model(x_val, num_samples=10)
+    mse_val = F.mse_loss(y_pred_val, y_val).mean().item()
+    writer.add_scalar("mse_val", mse_val, epoch)
+
+    if epoch % 200 == 0:
+        # plot results with all ground truth
+        y_pred, std_pred = model(x_all, num_samples=10)
+        fig = plot_results(x_train, y_train, x_all, y_all, y_pred, std_pred)
+        writer.add_figure("regression", fig, epoch)
+
     return mse_val
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--study", type=str, help="resume existing study", default="")
-    args = parser.parse_args()
-    project_dir = Path(__file__).resolve().parents[1]
+def train_1d_regression():
+    dim_h = 512
+    prior_sigma_1 = 9.39
+    prior_sigma_2 = 0.0001
+    prior_pi = 0.46
 
+    log_dir = (
+        project_dir
+        / f"runs/individual/{datetime.now().strftime('%Y%m%d_%H%M%S')}-dim_h_{dim_h}"
+          f"-s1_{prior_sigma_1:.2f}-s2_{prior_sigma_2:.2f}-pi_{prior_pi:.2f}"
+    )
+
+    x_train, y_train, x_val, y_val, x_all, y_all = get_toy_data()
+    dataloader_train = DataLoader(
+        TensorDataset(x_train, y_train), batch_size=16, shuffle=True
+    )
+
+    model = BayesianMLP(
+        dim_in=1,
+        dim_out=1,
+        dim_h=dim_h,
+        prior_sigma_1=prior_sigma_1,
+        prior_sigma_2=prior_sigma_2,
+        prior_pi=prior_pi,
+    )
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    return train(
+        model,
+        optimizer,
+        dataloader_train,
+        n_epochs=200,
+        log_dir=log_dir,
+        evaluate_func=eval_1d_regression,
+        evaluate_data=(x_train, y_train, x_val, y_val, x_all, y_all),
+    )
+
+
+def objective(trial: optuna.Trial):
+    dim_h = 512
+    prior_sigma_1 = trial.suggest_float("prior_sigma_1", low=1e-3, high=10.0, log=True)
+    prior_sigma_2 = trial.suggest_float("prior_sigma_2", low=1e-3, high=10.0, log=True)
+    prior_pi = trial.suggest_float("prior_pi", low=0.0, high=1.0)
+
+    log_dir = (
+        project_dir
+        / f"runs/{trial.study.study_name}/trial_{trial.number}-dim_h_{dim_h}-"
+        f"s1_{prior_sigma_1:.2f}-s2_{prior_sigma_2:.2f}-pi_{prior_pi:.2f}"
+    )
+
+    x_train, y_train, x_val, y_val, x_all, y_all = get_toy_data()
+    dataloader_train = DataLoader(
+        TensorDataset(x_train, y_train), batch_size=16, shuffle=True
+    )
+
+    model = BayesianMLP(
+        dim_in=1,
+        dim_out=1,
+        dim_h=dim_h,
+        prior_sigma_1=prior_sigma_1,
+        prior_sigma_2=prior_sigma_2,
+        prior_pi=prior_pi,
+    )
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    return train(
+        model,
+        optimizer,
+        dataloader_train,
+        n_epochs=500,
+        log_dir=log_dir,
+        evaluate_func=eval_1d_regression,
+        evaluate_data=(x_train, y_train, x_val, y_val, x_all, y_all),
+    )
+
+
+def make_hyper_param_study():
     if args.study == "":
         # create study
         study_name = f"study_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
@@ -121,3 +172,13 @@ if __name__ == "__main__":
 
     study.optimize(objective, n_trials=100)
     joblib.dump(study, project_dir / f"runs/{study_name}/study.pkl")
+
+
+if __name__ == "__main__":
+    project_dir = Path(__file__).resolve().parents[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--study", type=str, help="resume existing study", default="")
+    args = parser.parse_args()
+
+    make_hyper_param_study()
+    # train_1d_regression()
